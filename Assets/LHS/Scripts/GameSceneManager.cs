a30e1864865775d7dc7226e717a15141444a7b60
Geundol222 <geundol222@gmail.com>
2023-07-28 04:23:17 +0900

    feat : GameManager & GameSceneManager

---
+++ Assets/LHS/Scripts/GameSceneManager.cs
@@ -0,0 +1,135 @@
+using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameSceneManager : MonoBehaviourPunCallbacks
+{
+    [SerializeField] TMP_Text infoText;
+    [SerializeField] float countDownTimer;
+
+    [SerializeField] GameObject groundObj;
+
+    private List<GameObject> groundList;
+
+    private void Awake()
+    {
+        groundList = new List<GameObject>();
+    }
+
+    private void Start()
+    {
+        if (PhotonNetwork.InRoom)
+        {
+            PhotonNetwork.LocalPlayer.SetLoad(true);
+        }
+        else
+        {
+            infoText.text = "Debug Mode";
+            PhotonNetwork.LocalPlayer.NickName = $"DebugPlayer {Random.Range(1000, 10000)}";
+            PhotonNetwork.ConnectUsingSettings();
+        }
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        RoomOptions options = new RoomOptions() { IsVisible = false };
+        PhotonNetwork.JoinOrCreateRoom("DebugRoom", options, TypedLobby.Default);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        StartCoroutine(DebugGameSetupDelay());
+    }
+
+    IEnumerator DebugGameSetupDelay()
+    {
+        yield return new WaitForSeconds(1f);
+        DebugGameStart();
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.Log($"Disconnected : {cause}");
+        // SceneManager.LoadScene("LobbyScene");
+    }
+
+    public override void OnLeftRoom()
+    {
+        // PhotonNetwork.LoadLevel("LobbyScene");
+    }
+
+    public override void OnMasterClientSwitched(Player newMasterClient)
+    {
+        if (PhotonNetwork.IsMasterClient) { }
+            // TODO : 방장이 바꼈을 때
+    }
+
+    public override void OnPlayerPropertiesUpdate(Player targetPlayer, ExitGames.Client.Photon.Hashtable changedProps)
+    {
+        if (changedProps.ContainsKey("Load"))
+        {
+            if (PlayerLoadCount() == PhotonNetwork.PlayerList.Length)
+            {
+                if (PhotonNetwork.IsMasterClient)
+                    PhotonNetwork.CurrentRoom.SetLoadTime(PhotonNetwork.ServerTimestamp);
+            }
+            else
+            {
+                Debug.Log($"Wait Players {PlayerLoadCount()} / {PhotonNetwork.PlayerList.Length}");
+                infoText.text = $"Wait Players {PlayerLoadCount()} / {PhotonNetwork.PlayerList.Length}";
+            }
+        }
+    }
+
+    public override void OnRoomPropertiesUpdate(ExitGames.Client.Photon.Hashtable propertiesThatChanged)
+    {
+        if (propertiesThatChanged.ContainsKey("LoadTime"))
+        {
+            StartCoroutine(GameStartTimer());
+        }
+    }
+
+    IEnumerator GameStartTimer()
+    {
+        int loadTime = PhotonNetwork.CurrentRoom.GetLoadTime();
+        while (countDownTimer > (PhotonNetwork.ServerTimestamp - loadTime) / 1000f)
+        {
+            int remainTime = (int)(countDownTimer - (PhotonNetwork.ServerTimestamp - loadTime) / 1000f);
+            infoText.text = $"All Player Loaded,\nStart CountDown : {remainTime + 1}";
+            yield return new WaitForEndOfFrame();
+        }
+        infoText.text = "Game Start!";
+        GameStart();
+
+        yield return new WaitForSeconds(1f);
+        infoText.text = "";
+    }
+
+    private void GameStart()
+    {
+        // TODO : GameStart
+    }
+
+    private void DebugGameStart()
+    {
+        // TODO : Debug GameStart
+        for (int i = 0; i < groundObj.transform.childCount; i++)
+        {
+            groundList.Add(groundObj.transform.GetChild(i).gameObject);
+        }
+    }
+
+    private int PlayerLoadCount()
+    {
+        int loadCount = 0;
+        foreach (Player player in PhotonNetwork.PlayerList)
+        {
+            if (player.GetLoad())
+                loadCount++;
+        }
+        return loadCount;
+    }
+}
---
+++ Assets/LHS/Scripts/Managers/GameManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance;
+
+    public static GameManager Instance { get { return instance; } }
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(this);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(this);
+        InitManagers();
+    }
+
+    private void OnDestroy()
+    {
+        if (instance == this)
+            instance = null;
+    }
+
+    private void InitManagers()
+    {
+
+    }
+}
