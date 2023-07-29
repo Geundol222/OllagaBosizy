e90622cc5b18aef66373917da83f2370f50d8b06
Geundol222 <geundol222@gmail.com>
2023-07-29 22:43:17 +0900

    feat : PlatForm, TeamManager 삭제 & BaseUI 상속관계 설정

--- Assets/LHS/Scripts/UI/InGameUI/InGameUI.cs
+++ Assets/LHS/Scripts/UI/InGameUI/InGameUI.cs
@@ -2,17 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InGameUI : MonoBehaviour
+public class InGameUI : BaseUI
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform followTarget;
+    public Vector2 followOffset;
+
+    private void LateUpdate()
     {
-        
+        if (followTarget == null)
+            GameManager.UI.CloseInGameUI<InGameUI>(this);
+
+        if (followTarget != null)
+        {
+            transform.position = Camera.main.WorldToScreenPoint(followTarget.position) + (Vector3)followOffset;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetTarget(Transform target)
     {
-        
+        this.followTarget = target;
+        if (followTarget != null)
+        {
+            transform.position = Camera.main.WorldToScreenPoint(followTarget.position) + (Vector3)followOffset;
+        }
+    }
+
+    public void SetOffset(Vector2 offset)
+    {
+        this.followOffset = offset;
+        if (followTarget != null)
+        {
+            transform.position = Camera.main.WorldToScreenPoint(followTarget.position) + (Vector3)followOffset;
+        }
     }
 }
