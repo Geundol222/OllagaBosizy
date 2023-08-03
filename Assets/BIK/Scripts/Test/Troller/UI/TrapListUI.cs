783948321c6bcaee708cb15a5e1ef038d47deb9d
인권 백 <titanbaek91@gmail.com>
2023-08-03 21:36:32 +0900

    feat : 디버프 클래스,플랫폼 스크립트 내 기능들 디버프 매니저로 기능 분산

--- Assets/BIK/Scripts/Test/Troller/UI/TrapListUI.cs
+++ Assets/BIK/Scripts/Test/Troller/UI/TrapListUI.cs
@@ -1,27 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
-public class TrapListUI : InGameUI
+public class TrapListUI : GameSceneUI
 {
     private Debuff[] debuffs;
-
-    private void Awake()
-    {
-        Debug.Log("Awake");
-    }
+    private TMP_Text[] debuffTexts;
 
     private void Start()
     {
-        Debug.Log("Start");
+        debuffTexts = GetComponentsInChildren<TMP_Text>();
     }
+
     public void UpdateList(Debuff[] debuffs)
     {
         this.debuffs = debuffs;
+        SetTrapListUI();
     }
+
     public void SetTrapListUI()
     {
+        if (debuffs == null)
+            return;
+        if (debuffTexts == null)
+            debuffTexts = GetComponentsInChildren<TMP_Text>();
 
+        for (int i = 0; i < debuffTexts.Length; i++)
+        {
+            debuffTexts[i].text = debuffs[i].state.ToString();
+        }
     }
 
 }
