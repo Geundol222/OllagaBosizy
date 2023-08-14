b48f20e2aa2f48992f63f1453255c0e4c9c25cb6
KANG SOO IN <ks00in213@gmail.com>
2023-08-14 09:45:19 +0900

    feat : Troller Emoji 구현

---
+++ Assets/KSI/Scripts/UI/Emoji/Emoji.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Emoji : MonoBehaviour
+{
+	public EmojiData[] emojidatas;
+	public TextMeshProUGUI text;
+
+	private int randomIndex;
+	private string emojiText;
+
+	private void Awake()
+	{
+		StartCoroutine(DisplayRandomEmojiRoutine());
+	}
+
+	private IEnumerator DisplayRandomEmojiRoutine()
+	{
+		while (true)
+		{
+			if (text != null && emojidatas.Length > 0)
+			{
+				randomIndex = Random.Range(0, emojidatas.Length);
+				emojiText = emojidatas[randomIndex].emojiDescription;
+				text.text = emojiText;
+				Debug.Log(emojiText);
+			}
+			else
+			{
+				Debug.Log("GameTipData 할당되지 않음");
+			}
+
+			yield return new WaitForSeconds(1f);
+		}
+	}
+}
